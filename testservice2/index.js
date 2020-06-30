da4d270cacd88e568e9625c1640b399f8393b347
Akshans Verma <akshansv@gmail.com>
2020-06-29 21:08:46 -0500

    testing deployment again w/secrets updated

--- testservice2/index.js
+++ testservice2/index.js
@@ -3,12 +3,8 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res) {
-<<<<<<< HEAD
-  res.send('Hello World!');
-=======
-  res.send('Hello Worrsdfld!');
->>>>>>> 4a185e291dc61e3931b6d5407bd120e600cfa756
-});
+  res.send('Hello Wold!');
+
 
 app.listen(3000, function() {
   console.log('Example app liste ning on port 3000!');
